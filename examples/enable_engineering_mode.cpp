a52ad3c1314bf86ef4732b433e89138d8092d722
Dominik Schidlowski <dominik@schidlowski.eu>
2023-05-14 14:33:38 +0200

    expand example

--- examples/enable_engineering_mode.cpp
+++ examples/enable_engineering_mode.cpp
@@ -28,7 +28,7 @@ void setup(void) {
     while (!packet_reader.read().has_value());
 
     // each method sends a packet and waits for the correct answer.
-    commander.enable_configuration();
+    commander.enable_configuration(); // returns true for successfull, false if not
     commander.enable_engineering_mode();
     commander.end_configuration();
 }
@@ -38,11 +38,35 @@ void loop(void) {
   auto packet = packet_reader.read();
 
   if (packet.has_value()) {
+    // check packet name
+    if (packet->name() != PacketName::EngineeringModeDataFrame) {
+      return;
+    }
+
+    // or only the normal data frame which does not include the extra engineering mode fields
+    /*
+    if (packet->name() != PacketName::ReportingDataFrame) {
+      return;
+    }
+    */
+
     // print summary of packet
     Serial.println(packet->to_string().c_str());
 
     // print packet data as hex string
     Serial.println(packet->data_to_hexstring().c_str());
+
+    // grab single fields from packet
+    // have a look in ld2410_packet_definition.h at the bottom for information which fields are available for which packets
+    Serial.println(*packet->read_first_field(FieldName::movementTargetDistance));
+
+    // iterate over fields with the same name
+    for(uint32_t i = 0; i < *packet->read_first_field(FieldName::maximumMovingDistanceGateN); i++) {
+      Serial.println(*packet->read_nth_field(FieldName::movementDistanceGateEnergyValue, i));
+    }
+
+    // alternatively grab a vector of all fields with the same name
+    auto field_vector = packet->read_all_fields(FieldName::staticDistanceGateEnergyValue);
   }
 }
 
